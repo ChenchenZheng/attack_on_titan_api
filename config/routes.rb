21fd320b064d7b622aa16ac28263276fb3d10f8e
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-08 16:46:14 +0200

    ✨ Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :characters, only:[:index, :show, :create]
+      resources :characters, only:[:index, :show]
     end
   end
+
+  root to: 'pages#home'
 end
