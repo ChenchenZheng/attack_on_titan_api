506ec152b54f67bedafcfff09e3c0fa9d48fe97d
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-15 14:17:37 +0200

    ✨ Search by first name or last name

--- app/controllers/api/v1/characters_controller.rb
+++ app/controllers/api/v1/characters_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::CharactersController < ApplicationController
   before_action :set_character, only: [:show]
   def index
-    @characters = Character.all
+    if params[:query].present?
+      sql_query = "first_name ILIKE :query OR last_name ILIKE :query"
+      @characters = Character.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @characters = Character.all
+    end
   end
 
   def show
--- app/views/api/v1/characters/index.json.jbuilder
+++ app/views/api/v1/characters/index.json.jbuilder
@@ -1,3 +1,3 @@
 json.array! @characters do |character|
-  json.extract! character, :id, :first_name, :last_name
+  json.extract! character, :id, :first_name, :last_name, :species, :age, :height, :residence, :status, :alias
 end
