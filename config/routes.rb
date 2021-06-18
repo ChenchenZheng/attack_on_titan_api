236c37062164644d08d54f77160eec2bb6cb2faa
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-18 12:02:35 +0200

    ✨ Rails admin added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
