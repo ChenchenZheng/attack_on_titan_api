b2517ff8e587f7a02342059d444a9c9c38396fe8
Chenchen Zheng <chenchenzheng42@gmail.com>
2021-06-30 16:12:48 +0200

    ✨ Add single titan endpoint to API

--- app/controllers/api/v1/titans_controller.rb
+++ app/controllers/api/v1/titans_controller.rb
@@ -1,5 +1,16 @@
 class Api::V1::TitansController < Api::V1::BaseController
+  before_action :set_titan, only: [:show]
+
   def index
     @titans = Titan.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_titan
+    @titan = Titan.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :characters, only:[:index, :show]
-      resources :titans, only:[:index]
+      resources :titans, only:[:index, :show]
     end
   end
 
